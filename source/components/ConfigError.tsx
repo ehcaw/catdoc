5d4402570cae98238ee796bce3a799c854cf7995
daniel ung <daniel.ung@sjsu.edu>
2025-04-21 16:58:32 -0700

    menu page changes

--- source/components/ConfigError.tsx
+++ source/components/ConfigError.tsx
@@ -23,14 +23,25 @@ export const ConfigError: React.FC<ConfigErrorProps> = ({onBack}) => {
         <Box flexDirection="column" alignItems="center" padding={1}>
             <Text>
                 {`
-     /\\___/\\     😾
-    ( >w< )    
-    (  =^=  ) ~  Configuration needed!
-     (______)    
+            ___
+          (___)
+   ____
+ _\\___ \\  |\\_/|
+\\     \\ \\/ , , \\ ___
+ \\__   \\ \\ ="= //|||\\
+  |===  \\/____)_)||||
+  \\______|    | |||||
+      _/_|  | | =====
+     (_/  \\_)_) snd
+  _________________
+ (                _)
+  (__   '          )
+    (___    _____)
+        '--'
                 `}
             </Text>
             <Box marginY={1}>
-                <Text>Press <Text color="yellow">ESC</Text> to go back and configure your API key</Text>
+                <Text>Press <Text color="yellow">ESC</Text> to go back and configure your API key (ascii art by by Shanaka Dias) </Text>
             </Box>
         </Box>
     );
